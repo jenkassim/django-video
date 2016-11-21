255633b5b19da5474e7301a2cec74d864c13ee31
Santiago Basulto <santiago.basulto@gmail.com>
2016-11-21 18:19:36 -0300

    Added listing page

--- django_video/curated_videos/urls.py
+++ django_video/curated_videos/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from .views import HomePageView
+from .views import HomePageView, CategoryListingView, ConferenceListingView
 
 
 urlpatterns = [
     url(r'^$', HomePageView.as_view()),
-
+    url(r'^category/(?P<slug>[\w-]+)$', CategoryListingView.as_view(), name='category-listing'),
+    url(r'^conference/(?P<slug>[\w-]+)$', ConferenceListingView.as_view(), name='conference-listing'),
 ]
--- django_video/curated_videos/views.py
+++ django_video/curated_videos/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import TemplateView
-from .models import Video, HomeFeaturedVideo, Category
+from .models import Video, HomeFeaturedVideo, Category, Conference
+from django.shortcuts import get_object_or_404
+
 
 class HomePageView(TemplateView):
     template_name = 'index.html'
@@ -13,3 +15,30 @@ class HomePageView(TemplateView):
         context['home_featured_video'] = home_featured_video.video
         context['featured_categories'] = featured_categories
         return context
+
+
+class SlugListingView(TemplateView):
+    template_name = 'listing.html'
+    MODEL_CLASS = None
+
+    def get_context_data(self, **kwargs):
+        context = super(SlugListingView, self).get_context_data(**kwargs)
+        slug = kwargs['slug']
+        listing = get_object_or_404(self.MODEL_CLASS, slug=slug)
+        context['listing'] = listing
+        context['videos'] = self.get_videos(listing)
+        return context
+
+
+class CategoryListingView(SlugListingView):
+    MODEL_CLASS = Category
+
+    def get_videos(self, obj):
+        return obj.videos.all()
+
+
+class ConferenceListingView(SlugListingView):
+    MODEL_CLASS = Conference
+
+    def get_videos(self, obj):
+        return Video.objects.filter(conference=obj)
