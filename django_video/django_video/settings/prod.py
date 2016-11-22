362130efac356727ae3c721687c1610fa9c0e691
Santiago Basulto <santiago.basulto@gmail.com>
2016-11-22 08:29:02 -0300

    Allow heroku dev host on settings

--- django_video/django_video/settings/prod.py
+++ django_video/django_video/settings/prod.py
@@ -4,3 +4,5 @@ from .base import *
 SECRET_KEY = '^8ai-6gb!yygteoai8a%c=)mb0xler7%0klh1mz!^snago;91_'
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
+
+ALLOWED_HOSTS = ['afternoon-plateau-28499.herokuapp.com', 'django.video']
